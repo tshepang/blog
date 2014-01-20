d088d6a57c814b0147cab0267b1fe1e0bc933791
Tshepang Lekhonkhobe <tshepang@gmail.com>
2014-01-21 01:29:00 +0200

    one more plugin

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ STATIC_PATHS = ['images', 'CNAME']
 SOCIAL_PROFILE_LABEL = 'Stay in Touch'
 USE_FAVICON = True
 PLUGIN_PATH = '../pelican-plugins'
-PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'neighbors']
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'neighbors', 'assets']
 SITEMAP = {'format': 'xml'}
 DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives',
                      'search', '404'))
